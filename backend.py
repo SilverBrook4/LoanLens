e83252bdd921fc0d934e383b55c6152c9684320f
NicolasFay <njfay@uvm.edu>
2026-02-28 10:13:21 -0500

    updated file structure

--- backend.py
+++ backend.py
@@ -3,6 +3,15 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from database import db
 from typing import Optional
+import uvicorn
 
 app = FastAPI()
-app.mount("/frontend", StaticFiles(directory="frontend"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def login(request: Request):
+    return 
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
